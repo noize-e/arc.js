2469f14e7c11ab9ce2849510b537d12dba766821
fnk <me@nzr.local>
2020-12-10 19:47:21 -0600

    added new log mehod debug

--- src/proto/arc.js
+++ src/proto/arc.js
@@ -4,37 +4,39 @@
 
     var debug = true,
         conf = {
-        stdout: true,
-        utils: {
-            global: true
-        },
-        xclient: {
-            httpMethods: ["GET", "POST", "PUT", "DELETE"],
-            contentType: "application/json",
-            dataType: "json",
-            headers: {},
-            gateway: {
-                key: null,
-                invoke_url: null
-            }
-        },
-        session: {
-            ctx_glob: false
-        },
-        cognito: {
-            poolId: null,
-            region: "us-east-1",
-            domain: null,
-            clientId: null,
-            endpoints: {
-                redirectUri: null,
-                authorize: {
-                    path: "/oauth2/authorize",
-                    qs: "response_type=code&scope=openid+profile+aws.cognito.signin.user.admin&state=STATE"
+            stdout: {
+                debug: true
+            },
+            utils: {
+                global: true
+            },
+            xclient: {
+                httpMethods: ["GET", "POST", "PUT", "DELETE"],
+                contentType: "application/json",
+                dataType: "json",
+                headers: {},
+                gateway: {
+                    key: null,
+                    invoke_url: null
+                }
+            },
+            session: {
+                ctx_glob: false
+            },
+            cognito: {
+                poolId: null,
+                region: "us-east-1",
+                domain: null,
+                clientId: null,
+                endpoints: {
+                    redirectUri: null,
+                    authorize: {
+                        path: "/oauth2/authorize",
+                        qs: "response_type=code&scope=openid+profile+aws.cognito.signin.user.admin&state=STATE"
+                    }
                 }
             }
-        }
-    };
+        };
 
     // @public
     arc.get_conf = function(modname){
@@ -51,29 +53,33 @@
 
 
     function log(level, msg, data, type){
-        if(debug){
-            var action = "log";
-            if(level == 2){
-                action = "warn";
-            } else if(level == 3){
-                action = "error";
-            }
+        var action = "log";
+        if(level == 2){
+            action = "warn";
+        } else if(level == 3){
+            action = "error";
+        }
 
-            var label = "[$]".replace("$", action);
-            if(arc.isSet(data) && arc.isObj(data)){
-                console.group(label)
-                console[action](":: %s ::", msg);
-                console[(type || "table")](data);
-                console.groupEnd(label)
-            }else{
-                console[action]("%s :: %s %s ::", label, msg, (data || ""));
-            }
+        var label = "[$]".replace("$", action);
+        if(arc.isSet(data) && arc.isObj(data)){
+            console.group(label)
+            console[action](":: %s ::", msg);
+            console[(type || "table")](data);
+            console.groupEnd(label)
+        }else{
+            console[action]("%s :: %s %s ::", label, msg, (data || ""));
         }
     }
 
     // @public
     arc.log = function(msg, data, type) {
         log(1, msg, data, type)
+    }
+
+    arc.debug = function(msg, data, type) {
+        if((arc.get_conf("stdout")).debug){
+            log(1, msg, data, type)
+        }
     }
 
     arc.warn = function(msg, data, type) {
