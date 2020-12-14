81946b1f5ae7d03ce5ccdf94a0b39cf1a7594948
fnk <me@nzr.local>
2020-12-14 15:10:35 -0600

    modified owns detection method, now receives a string or a strings array

--- src/proto/utils.js
+++ src/proto/utils.js
@@ -23,6 +23,16 @@
             }
         }
 
+        /**
+         * Validators:
+         *  isNull     {*}
+         *  isSet      {*}
+         *  isFunc     {*}
+         *  isObj      {*}
+         *  isEmail    {*}
+         *  isArray    {*}
+         */
+
         function isNull(obj) {
             return obj === null;
         }
@@ -33,19 +43,17 @@
             return typeof obj !== "undefined";
         }
 
-        // new
         function isFunc(func){
             return Object.prototype.toString.call(func) === "[object Function]";
         }
 
-        // new
         function isObj(obj){
             return Object.prototype.toString.call(obj) === "[object Object]";
         }
 
-        // new
-        function owns(obj, prop){
-            return obj.hasOwnProperty(prop);
+        function isEmail(ve) {
+            var flt = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return flt.test(ve);
         }
 
         function isArray(obj) {
@@ -56,57 +64,34 @@
             }
         }
 
-        function numberOnly(element) {
-            /**
-             * This removes any other character but numbers as entered by user
-             *
-             * Usage:
-             *  <input type="text" oninput="arc.u.numberOnly(this);" maxlength="3" />
-             */
-            element.value = element.value.replace(/[^0-9]/gi, "");
-        }
 
-        function _extend(left, right, merge) {
-            Object.keys(right).forEach(function(k) {
-                if (owns(right, k) && !isSet(merge) && !isObj(right[k])) {
-                    left[k] = right[k];
-                }
-                else if (owns(right, k) && isSet(merge) && !isObj(right[k])) {
-                    left[k] = right[k];
-                }
-                else {
-                    _extend(left[k], right[k]);
-                }
-            });
-        }
+        /**
+         * Detection:
+         */
 
-        function goTo(uri, conf) {
-            var _url = '/' + uri,
-                _conf = {
-                    ext: false,
-                    delay: false
-                }
-
-            _extend(_conf, (conf || {}))
-
-            if(_conf.ext){
-                _url = uri;
+        /**
+         *
+         * Shortcut method for Object.prototype.hasOwnProperty
+         *
+         * @param {string} property The property key string to detect
+         * @return {boolean} if passes the test or not.
+         */
+        function owns(obj, arg){
+            var v = 0;
+            if(isArray(arg)){
+                arg.forEach(function(prop) {
+                    if(obj.hasOwnProperty(prop))
+                        v++
+                })
+                return (arg.length == v)
             }
 
-            try {
-                if(_conf.delay){
-                    setTimeout(function(){
-                        window.location.href = _url;
-                    }, parseInt(_conf.delay));
-                }else{
-                    window.location.href = _url;
-                }
-            } catch (err) {
-                stdout.warn(err);
-            }
-
-            return _url;
+            return obj.hasOwnProperty(arg)
         }
+
+        /**
+         * Retrieval:
+         */
 
         function getUrlVars() {
             var vars = {};
@@ -145,10 +130,6 @@
             return str.join("&");
         }
 
-        function isEmail(ve) {
-            var flt = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-            return flt.test(ve);
-        }
 
         function deepCopy(obj){
             return JSON.parse(JSON.stringify(obj))
@@ -157,13 +138,21 @@
 
         function hashCode(s) {
           var h = 0, l = s.length, i = 0;
+
+          // given that a string number is hashed
+          // to 0 return it untouched
+          if(!isNaN(s))
+            return s;
+
           if ( l > 0 )
             while (i < l)
               h = (h << 5) - h + s.charCodeAt(i++) | 0;
           return h;
         };
 
-
+        /**
+         * Only works with nodejs or a polyfill should be implemented
+         */
         function bufferEncoding(str, _from, _to){
             return Buffer.from(str, _from).toString(_to)
         }
@@ -175,6 +164,62 @@
             decode: function (str) {
                 return bufferEncoding(str, 'base64', 'binary');
             }
+        }
+
+        function numberOnly(element) {
+            /**
+             * This removes any other character but numbers as entered by user
+             *
+             * Usage:
+             *  <input type="text" oninput="arc.u.numberOnly(this);" maxlength="3" />
+             */
+            element.value = element.value.replace(/[^0-9]/gi, "");
+        }
+
+
+        function goTo(uri, conf) {
+            var _url = '/' + uri,
+                _conf = {
+                    ext: false,
+                    delay: false
+                }
+
+            _extend(_conf, (conf || {}))
+
+            if(_conf.ext){
+                _url = uri;
+            }
+
+            try {
+                if(_conf.delay){
+                    setTimeout(function(){
+                        window.location.href = _url;
+                    }, parseInt(_conf.delay));
+                }else{
+                    window.location.href = _url;
+                }
+            } catch (err) {
+                stdout.warn(err);
+            }
+
+            return _url;
+        }
+
+        /*
+         * In deprecation, it will be replaced by Object.assign(polifyll)
+         */
+        function _extend(left, right, merge) {
+            Object.keys(right).forEach(function(k) {
+                if (owns(right, k) && !isSet(merge) && !isObj(right[k])) {
+                    left[k] = right[k];
+                }
+                else if (owns(right, k) && isSet(merge) && !isObj(right[k])) {
+                    left[k] = right[k];
+                }
+                else {
+                    _extend(left[k], right[k]);
+                }
+            });
         }
 
 
